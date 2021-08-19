542ea6f270c29bdb4d2f7d2f931e05145c2ffca2
김현우 <r2872@naver.com>
2021-08-19 10:39:34 +0900

    버튼추가 4

--- app/src/main/java/com/example/a0819_01/MainActivity.kt
+++ app/src/main/java/com/example/a0819_01/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.a0819_01
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.a0819_01.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,11 +22,19 @@ class MainActivity : AppCompatActivity() {
         binding.logBtn01.setOnClickListener {
 //            무슨 말을 적어도 괜찮다, (메모 - 주석)
 //            Ctrl + / : 주석 단축키
-            Log.d("mainActivity","첫번째 로그 버튼이 눌렸습니다.")
+            Log.d("mainActivity", "첫번째 로그 버튼이 눌렸습니다.")
         }
 
         binding.logBtn02.setOnClickListener {
-            Log.d("mainActivity","두번째 로그 버튼이 눌렸습니다.")
+            Log.d("mainActivity", "두번째 로그 버튼이 눌렸습니다.")
+        }
+
+        binding.toastBtn01.setOnClickListener {
+            Toast.makeText(this, "토스트 메시지 버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.snackBtn01.setOnClickListener {
+            Snackbar.make(view, "스낵 바 띄우기", Snackbar.LENGTH_SHORT).show()
         }
     }
 
