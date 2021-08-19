38171779435522175b548d10f9bcd8fe63b66bd7
김현우 <r2872@naver.com>
2021-08-19 10:12:52 +0900

    버튼추가 2

--- app/src/main/java/com/example/a0819_01/MainActivity.kt
+++ app/src/main/java/com/example/a0819_01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a0819_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.a0819_01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,16 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.logBtn01.setOnClickListener {
+//            무슨 말을 적어도 괜찮다, (메모 - 주석)
+//            Ctrl + / : 주석 단축키
+            Log.d("mainActivity","첫번째 로그 버튼이 눌렸습니다.")
+        }
+
+        binding.logBtn02.setOnClickListener {
+            Log.d("mainActivity","두번째 로그 버튼이 눌렸습니다.")
+        }
     }
 
     override fun onDestroy() {
